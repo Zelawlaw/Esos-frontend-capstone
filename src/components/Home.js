1d6c53d9911e02f33a03c46656a861e8287001b3
Lawrence Karani <lawrencekarani@gmail.com>
2023-08-01 16:00:39 +0300

    made the summary section responsive to the number of tickets

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import axios from 'axios';
  
 function Home() {
   const [data, setData] = useState(null);
-
+  const [summary, setSummary] = useState(null);
   useEffect(() => {
     const instance = axios.create({
       baseURL: 'http://localhost:8080/api/v1/',
@@ -17,7 +17,6 @@ function Home() {
     const getIncidents = async () => {
       try {
         const response = await instance.get('getincidents');
-        console.log(JSON.stringify(response.data));
         return response.data;
       } catch (error) {
         console.error('Failed to fetch incidents', error);
@@ -28,23 +27,89 @@ function Home() {
     getIncidents().then(data => {
       setData(data);
     }).catch(error => {
-      console.log(error);
+      console.error(error);
     });
+
+
+    const getIncidentsSummary = async () => {
+      try {
+        const response = await instance.get('getincidentsummary');
+        return response.data;
+      } catch (error) {
+        console.error('Failed to fetch incident summary', error);
+        return null;
+      }
+    };
+ 
+    getIncidentsSummary().then( data => {
+      setSummary(data)
+    }).catch(error =>{
+      console.error(error)
+    })
+
 
   }, []);
 
-  if (!data) {
+  if (!data || !summary) {
     return "Loading...";
   }
 
   const { personalIncidents, reporteeIncidents } = data;
 
   return (
+
+    
+
     <div className='px-3'>
       <Nav />
       <div className='container-fluid'>
-        {/* The rest of your content goes here, this is where you could use the getIncidentSummary data */}
-      </div>
+            <div className='row g-3 my-2'>
+                <div className='col-md-3 p-1'>
+                    <div className='p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded'>
+                        <div>
+                            <p className='fs-4'>All</p>
+                            <h3 className='fs-4'>{summary.all}</h3>
+                            
+                        </div>
+                        <i className='bi bi-circle-fill p-3 fs-1 text-danger'></i>
+                    </div>
+                </div>
+
+                <div className='col-md-3 p-1'>
+                    <div className='p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded'>
+                        <div>
+                            <p className='fs-4'>Active</p>
+                            <h3 className='fs-4'>{summary.active}</h3>
+                            
+                        </div>
+                        <i className='bi bi-circle-fill p-3 fs-1 text-warning'></i>
+                    </div>
+                </div>
+
+                <div className='col-md-3 p-1'>
+                    <div className='p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded'>
+                        <div>
+                            <p className='fs-4'>Pending</p>
+                            <h3 className='fs-4'>{summary.pending}</h3>
+                            
+                        </div>
+                        <i className='bi bi-circle-fill p-3 fs-1 text-warning'></i>
+                    </div>
+                </div>
+
+                <div className='col-md-3 p-1'>
+                    <div className='p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded'>
+                        <div>
+                            <p className='fs-4'>Resolved</p>
+                            <h3 className='fs-4'>{summary.resolved}</h3>
+                            
+                        </div>
+                        <i className='bi bi-circle-fill p-3 fs-1 text-success'></i>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
       {personalIncidents && personalIncidents.length > 0 && (
         <table className="table table-hover caption-top bg-white rounded mt-2">
