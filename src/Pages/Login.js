aa5b38aa7e604d9b60d445dc5c4902b1c8a34e02
Lawrence Karani <lawrencekarani@gmail.com>
2023-08-01 10:59:51 +0300

    added axios, jwt-decode, added loginservice and stitched to loginpage

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,57 +1,73 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
+import loginService from "../services/loginService"; 
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState(""); 
 
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userData = await loginService.login(username, password);
+      setUsername(userData.username);
+      setRole(userData.role);
+      console.log("Successful login");
+      navigate("/dashboard");
+    } catch (error) {
+      console.log("Failed login");
+    }
+  };
 
-// export const Login = () => {
-//     return {
-//         <Login />
-//     };
-// }
+  return (
+    <>
+      {/* <p> Hey</p>;  */}
 
-function Login () {
+      <div className="d-flex vh-100 justify-content-center align-items-center bg-success">
+        <div className="p-3 bg-white w-25">
+          <div className="big-title text-center">
+            <h2>eSOS</h2>
+          </div>
+          <form action="">
+            <div className="mb-3">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                placeholder="Your username"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)} 
 
-    // eslint-disable-next-line
-    // const [email, setEmail] = useState('');
-    // const [pass, setPass] = useState('');
-
-    const navigate = useNavigate();
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        navigate ('/dashboard')
-        console.log("Successful login");
-    } 
-
-    return (
-        <>
-        {/* <p> Hey</p>;  */}
-
-            <div className="d-flex vh-100 justify-content-center align-items-center bg-success">
-            <div className="p-3 bg-white w-25">
-            <div className="big-title text-center"><h2>eSOS</h2></div>
-            <form action="">
-                <div className="mb-3">
-                    <label for="email">Email</label>
-                    <input type="email" placeholder="xxxxx@safaricom.co.ke" className="form-control" />
-                </div>
-
-                <div className="mb-3">
-                    <label for="password">Password</label>
-                    <input type="password" placeholder="xxxxxxxxxxx" className="form-control"/>
-                </div>
-           
-                <button className="btn btn-success" type="submit" onClick={handleSubmit}> Log In </button>
-            </form>
+              />
             </div>
+
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="xxxxxxxxxxx"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
+              />
+            </div>
+
+            <button
+              className="btn btn-success"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              {" "}
+              Log In{" "}
+            </button>
+          </form>
         </div>
-
-       
-
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
