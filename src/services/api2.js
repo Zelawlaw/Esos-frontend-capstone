e64ec06f1a31a33b4303b9dcc6e6846a5b92c9ef
Lawrence Karani <lawrencekarani@gmail.com>
2023-08-03 17:10:56 +0300

    backend url in https

--- src/services/api2.js
+++ src/services/api2.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getAxiosInstance = (bearer) => {
   return axios.create({
-    baseURL: 'http://esos-backend-3b1dcc562da0.herokuapp.com/api/v1/',
+    baseURL: 'https://esos-backend-3b1dcc562da0.herokuapp.com/api/v1/',
     timeout: 5000,
     headers: { 'Authorization': `Bearer ${bearer}` },
   });
