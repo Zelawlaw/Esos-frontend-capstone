cebab3f104bb04ba038c532109153cde821324db
Lawrence Karani <lawrencekarani@gmail.com>
2023-08-04 10:54:30 +0300

    changed backend url

--- src/services/api2.js
+++ src/services/api2.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getAxiosInstance = (bearer) => {
   return axios.create({
-    baseURL: 'https://esos-backend-3b1dcc562da0.herokuapp.com/api/v1/',
+    baseURL: 'https://esos-backend-0c90497ee6b3.herokuapp.com/api/v1/',
     timeout: 5000,
     headers: { 'Authorization': `Bearer ${bearer}` },
   });
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,21 +1,25 @@
-
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
-
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 
 const login = async (username, password) => {
-    
-    try {
-        const response = await axios.post('https://esos-backend-3b1dcc562da0.herokuapp.com/api/v1/authenticate', { username, password });
-        const decodedjwt = jwtDecode(response.data.token);
-        console.log('decoded jwt:'+JSON.stringify(decodedjwt));
-        // Store token in the local storage
-        localStorage.setItem('jwtToken', response.data.token);
-        
-        return { token:response.data.token, username: decodedjwt.sub, role: decodedjwt.roles[0] };
-    } catch (error) {
-        throw new Error('Login failed');
-    }
+  try {
+    const response = await axios.post(
+      "https://esos-backend-0c90497ee6b3.herokuapp.com/api/v1/authenticate",
+      { username, password }
+    );
+    const decodedjwt = jwtDecode(response.data.token);
+    console.log("decoded jwt:" + JSON.stringify(decodedjwt));
+    // Store token in the local storage
+    localStorage.setItem("jwtToken", response.data.token);
+
+    return {
+      token: response.data.token,
+      username: decodedjwt.sub,
+      role: decodedjwt.roles[0],
+    };
+  } catch (error) {
+    throw new Error("Login failed");
+  }
 };
 
 const loginService = {
